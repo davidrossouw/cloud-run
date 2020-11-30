58c110f3612cd7578573ec7ba2ff2d899522b761
davidrossouw <davidrossouw@gmail.com>
2020-11-30 14:23:15 -0500

    live minimax search

--- connect4/app.py
+++ connect4/app.py
@@ -6,21 +6,17 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 
-from game import moves_available, play, checkWin, board2string
+from game import moves_available, play, checkWin, board2string, search, minimax
 
 app = Flask(__name__)
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+MAX_DEPTH = 7
+
 Session(app)
-
-
-
-# Load ttt lookup table
-MOVES_PATH = './connect4_minimax_1.json'
-with open(MOVES_PATH, 'r') as f:
-    moves = json.load(f)
 
 
 @app.route("/")
@@ -28,31 +24,28 @@ def index():
     # Initial state
     if "board" not in session:
         session["board"] = [
-            ['-','-','-','-'],
-            ['-','-','-','-'],
-            ['-','-','-','-'],
-            ['Y','R','Y','R']
+            ['-', '-', '-', '-'],
+            ['-', '-', '-', '-'],
+            ['-', '-', '-', '-'],
+            ['-', '-', '-', '-']
         ]
         session["turn"] = "Y"
         session["winner"] = None
         return render_template("game.html", board=session["board"], winner=session["winner"])
 
-    
-    # Computer turn
+    # Computer turn (Red='R')
     session['winner'] = checkWin(session['board'])
     # Check if player one with their move
     if session['winner'] in ('Y', 'R'):
-        #TODO: tidy up game=winner
+        # TODO: tidy up game=winner
         return render_template("game.html", board=session["board"], winner=session["winner"])
+    # Minimax
+    G = search(board=session["board"], max_depth=MAX_DEPTH)
+    G, move = minimax(G)
 
-    board_string = board2string(session["board"])
-    print('board_string lookup:', board_string)
-    move = moves.get(board_string, None)
     if move == None:
-        print('No best move!!')
+        print('best move not found!')
         move = random.choice(moves_available(session['board']))
-    
-    print(f'computer move: {move}')
 
     session['board'] = play(board=session['board'], player='R', column=move)
 
@@ -66,11 +59,7 @@ def index():
 
 @app.route("/player_move/<int:column>")
 def player_move(column):
-
     session['board'] = play(board=session['board'], player='Y', column=column)
-
-    print(board2string(session['board']))
-
     return redirect(url_for("index"))
 
 
@@ -85,4 +74,3 @@ if __name__ == '__main__':
     # a webserver process such as Gunicorn will serve the app.
     app.run(host='localhost', port=int(
         os.environ.get('PORT', 8080)), debug=True, use_reloader=True)
-
--- connect4/game.py
+++ connect4/game.py
@@ -1,19 +1,24 @@
 import copy
+import networkx as nx
 
 
 def columns(board):
     return len(board[0])
 
+
 def rows(board):
-    return len(board)   
+    return len(board)
+
 
 def moves_available(board) -> list:
     return [c for c in range(columns(board)) if board[0][c] == '-']
 
-def play(board, player:str, column:int):
-        
+
+def play(board, player: str, column: int):
+
     column_values = [row[column] for row in board]
-    row = max([i for i,v in enumerate(column_values) if v=='-']) # the row to which the chip will fall
+    # the row to which the chip will fall
+    row = max([i for i, v in enumerate(column_values) if v == '-'])
     new_board = copy.deepcopy(board)
     new_board[row][column] = player
     return new_board
@@ -24,23 +29,23 @@ def fourInARow(row, col, board):
     '''
     if board[row][col] == '-':
         return None
-    
-    # vertical down: 
+
+    # vertical down:
     if row < len(board)-3:
         if (board[row][col] == board[row+1][col] == board[row+2][col] == board[row+3][col]):
             return board[row][col]
-    
+
     # horizontal right:
     if col < len(board[0])-3:
         if (board[row][col] == board[row][col+1] == board[row][col+2] == board[row][col+3]):
             return board[row][col]
 
-    # right diag: 
+    # right diag:
     if (row < len(board)-3) & (col < len(board[0])-3):
         if (board[row][col] == board[row+1][col+1] == board[row+2][col+2] == board[row+3][col+3]):
             return board[row][col]
 
-    # left diag: 
+    # left diag:
     if (row < len(board)-3) & (col > 2):
         if (board[row][col] == board[row+1][col-1] == board[row+2][col-2] == board[row+3][col-3]):
             return board[row][col]
@@ -53,7 +58,7 @@ def checkWin(board):
     for row in range(rows(board)):
         for col in range(columns(board)):
             check_win = fourInARow(row, col, board)
-            if check_win: 
+            if check_win:
                 return check_win
 
     return None
@@ -65,9 +70,191 @@ def board2string(board):
         board_string.append(''.join([c for c in row]))
     return '\n'.join(board_string)
 
+
 def string2board(board_string):
     board = []
     for row in board_string.split('\n'):
         board.append([c for c in row])
     return board
 
+
+def columns(board):
+    return len(board[0])
+
+
+def rows(board):
+    return len(board)
+
+
+def moves_available(board) -> list:
+    return [c for c in range(columns(board)) if board[0][c] == '-']
+
+
+def play(board, player: str, column: int):
+    column_values = [row[column] for row in board]
+    # the row to which the chip will fall
+    row = max([i for i, v in enumerate(column_values) if v == '-'])
+    new_board = copy.deepcopy(board)
+    new_board[row][column] = player
+    return new_board
+
+
+def fourInARow(row, col, board):
+    '''
+    '''
+    if board[row][col] == '-':
+        return None
+
+    # vertical down:
+    if row < len(board)-3:
+        if (board[row][col] == board[row+1][col] == board[row+2][col] == board[row+3][col]):
+            return board[row][col]
+
+    # horizontal right:
+    if col < len(board[0])-3:
+        if (board[row][col] == board[row][col+1] == board[row][col+2] == board[row][col+3]):
+            return board[row][col]
+
+    # right diag:
+    if (row < len(board)-3) & (col < len(board[0])-3):
+        if (board[row][col] == board[row+1][col+1] == board[row+2][col+2] == board[row+3][col+3]):
+            return board[row][col]
+
+    # left diag:
+    if (row < len(board)-3) & (col > 2):
+        if (board[row][col] == board[row+1][col-1] == board[row+2][col-2] == board[row+3][col-3]):
+            return board[row][col]
+    return None
+
+
+def checkWin(board):
+    '''
+    '''
+    for row in range(rows(board)):
+        for col in range(columns(board)):
+            check_win = fourInARow(row, col, board)
+            if check_win:
+                return check_win
+
+    return None
+
+
+def search(board, max_depth=4):  # -> DiGraph
+    """
+    Run game simulations from current game state to a maximum number
+    of moves ahead (max_depth)
+    Return the graph of possible moves and outcomes
+    Assume current player is player to be maximized
+    """
+
+    depth = 0
+    n = 0  # node label which also serves as a node counter
+    player = 'R'
+    non_player = 'Y'
+    G = nx.DiGraph()
+    winner = checkWin(board)
+    G.add_node(0, winner=winner, board=board, n=n)
+    # First branch in look ahead
+    child_nodes = []
+
+    for move in moves_available(board):
+        # Do move
+        new_board = play(board, player=player, column=move)
+        winner = checkWin(new_board)
+        # Add move node to graph
+        n = n+1
+        G.add_node(n, winner=winner, board=new_board, n=n)
+        G.add_edge(0, n, move=move, player=player)
+
+        if winner:
+            ##
+            print(f'Winner: {winner}, {n}')
+            continue
+        child_nodes.append(n)
+
+    depth += 1
+    # Subsequent branches
+    while depth < max_depth:
+        # switch turns
+        player, non_player = non_player, player
+        child_node_subtree = child_nodes[:]
+        child_nodes = []
+        for child in child_node_subtree:
+            # Get parent state
+            parent_board = G.nodes(data=True)[child]['board']
+            for move in moves_available(parent_board):
+                # Do move
+                new_board = play(parent_board, player=player, column=move)
+                winner = checkWin(new_board)
+                # Add move node to graph
+                n = n+1
+                G.add_node(n, winner=winner, board=new_board, n=n)
+                G.add_edge(child, n, move=move, player=player)
+                if winner:
+                    continue
+                child_nodes.append(n)
+        depth = depth+1
+    return G
+
+
+def minimax(G: nx.Graph):
+    """
+    Perform minimax from node n on a NetworkX graph G.
+
+    Return graph with scores for moves and best move
+    """
+    maxplayer = True
+    minplayer = False
+
+    G = G.copy()
+    G.nodes[0].update({'player': 'max'})
+
+    # Recursive tree search
+    def _minimax(G, n, player):
+
+        # Base case, winning node found
+        if G.out_degree(n) == 0:
+            if G.nodes[n]['winner'] == 'R':
+                score = 100
+            elif G.nodes[n]['winner'] == 'Y':
+                score = -200
+            else:
+                score = 0
+            G.nodes[n].update({'score': score})
+            return score
+
+        if player == maxplayer:
+            bestv = -1
+            for child in G.successors(n):
+                v = _minimax(G, child, minplayer)
+                G.nodes[child].update({'score': v, 'player': 'min'})
+                bestv = max(bestv, v)
+        else:
+            bestv = 1
+            for child in G.successors(n):
+                v = _minimax(G, child, maxplayer)
+                G.nodes[child].update({'score': v, 'player': 'max'})
+                bestv = min(bestv, v)
+        return bestv
+
+    # Find the best first move from the given node
+    # Assume given node n is a maximiser node.
+    best_node = None
+    bestv = -1
+
+    for child in G.successors(0):
+        v = _minimax(G, child, minplayer)
+        G.nodes[child].update({'score': v, 'player': 'min'})
+        if v > bestv:
+            best_node = child
+            bestv = v
+
+    # Add best minimax move to each node
+    for n in G.nodes():
+        scores = [(v, c['move'], G.nodes[v]['score'])
+                  for (u, v, c) in G.out_edges(n, data=True)]
+        if scores:
+            best_move = max(scores, key=lambda t: t[2])[1]
+            G.nodes[n]['best_move'] = best_move
+
+    return G, G.nodes[0]['best_move']
